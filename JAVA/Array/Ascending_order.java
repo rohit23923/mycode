73432507bfcd212cc0025f68961ec83fc359b7cc
rohit23923 <r9416123923@gmail.com>
2023-11-01 14:28:00 +0530

    new try

--- JAVA/Array/Ascending_order.java
+++ JAVA/Array/Ascending_order.java
@@ -2,6 +2,7 @@ import java.util.*;
 public class Ascending_order {
     public static void main(String[] args)
     {
+        System.out.println();
         Scanner s=new Scanner(System.in);
         System.out.println("enter a Array");
         int[] a=new int[7] ;
